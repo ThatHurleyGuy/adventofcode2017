5cd7a0896438a8310b859c463ba55d6d4b91d524
hurley <sean.hurley6@gmail.com>
2017-12-01 11:50:30 -0700

    Clean up day 1

--- lib/adventofcode2017/day1.ex
+++ lib/adventofcode2017/day1.ex
@@ -2,35 +2,24 @@ defmodule Day1 do
   def sum(string) do
     list = String.graphemes(string)
 
-    {sum, last} = Enum.reduce(list, {0, nil}, fn(next, {sum, last_char}) ->
-      if last_char do
-        if last_char == next do
-          {sum + String.to_integer(last_char), next}
-        else
-          {sum, next}
-        end
-      else
-        {sum, next}
-      end
-    end)
-
-    if last == hd(list) do
-      sum + String.to_integer(last)
-    else
-      sum
-    end
+    sum(list, 1)
   end
 
   def sum_circle(string) do
     list = String.graphemes(string)
-    length = length(list)
-    halfway = length(list) / 2
+    halfway = round(length(list) / 2)
 
-    Enum.with_index(list)
-    |> Enum.reduce(0, fn({next, index}, sum) ->
-      halfway_around = Enum.at(list, rem(round(index + halfway), length))
-      if halfway_around == next do
-        sum + String.to_integer(next)
+    sum(list, halfway)
+  end
+
+  def sum(list, distance) do
+    {first_part, second_part} = Enum.split(list, distance)
+    rotated = second_part ++ first_part
+
+    Enum.zip(list, rotated)
+    |> Enum.reduce(0, fn({current, halfway_around}, sum) ->
+      if halfway_around == current do
+        sum + String.to_integer(current)
       else
         sum
       end
