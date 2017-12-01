c8cf7c62563ab642c5484b02901c98974a5ed1ab
hurley <sean.hurley6@gmail.com>
2017-12-01 11:45:49 -0700

    First pass at day1

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Adventofcode2017.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      escript: [main_module: AdventOfCode2017, name: "adventofcode"],
       deps: deps()
     ]
   end
