fafa97a5da838f59cd65cae88b13e61bd0144f99
hurley <sean.hurley6@gmail.com>
2017-12-03 19:44:15 -0700

    Day 2

--- lib/adventofcode2017/day2.ex
+++ lib/adventofcode2017/day2.ex
@@ -10,4 +10,33 @@ defmodule Day2 do
       sum + Enum.max(line) - Enum.min(line)
     end)
   end
+
+  def checksum_division(file) do
+    {:ok, contents} = File.read(file)
+    String.trim(contents)
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&(String.split(&1, ~r{\s+})))
+    |> Enum.reduce(0, fn(line, sum) ->
+      line = Enum.map(line, &String.to_integer/1)
+      sum + check_line_division(line)
+    end)
+  end
+
+  defp check_line_division(line) do
+    Enum.find_value(line, fn(dividend) ->
+      Enum.find_value(line, fn(divisor) ->
+        if dividend > divisor do
+          quotient = dividend / divisor
+          if quotient == round(quotient) do
+            quotient
+          else
+            nil
+          end
+        else
+          nil
+        end
+      end)
+    end)
+  end
 end
