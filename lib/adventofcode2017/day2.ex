0f0bf9964a3a268ae89ffd3a13ee5d92ad762fe9
hurley <sean.hurley6@gmail.com>
2017-12-03 19:48:16 -0700

    Day 2 cleanup

--- lib/adventofcode2017/day2.ex
+++ lib/adventofcode2017/day2.ex
@@ -1,25 +1,13 @@
 defmodule Day2 do
   def checksum(file) do
-    {:ok, contents} = File.read(file)
-    String.trim(contents)
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&(String.split(&1, ~r{\s+})))
-    |> Enum.reduce(0, fn(line, sum) ->
-      line = Enum.map(line, &String.to_integer/1)
-      sum + Enum.max(line) - Enum.min(line)
+    process_file(file, fn(line) ->
+      Enum.max(line) - Enum.min(line)
     end)
   end
 
   def checksum_division(file) do
-    {:ok, contents} = File.read(file)
-    String.trim(contents)
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&(String.split(&1, ~r{\s+})))
-    |> Enum.reduce(0, fn(line, sum) ->
-      line = Enum.map(line, &String.to_integer/1)
-      sum + check_line_division(line)
+    process_file(file, fn(line) ->
+      check_line_division(line)
     end)
   end
 
@@ -37,6 +25,18 @@ defmodule Day2 do
           nil
         end
       end)
+    end)
+  end
+
+  defp process_file(file, f) do
+    {:ok, contents} = File.read(file)
+    String.trim(contents)
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&(String.split(&1, ~r{\s+})))
+    |> Enum.reduce(0, fn(line, sum) ->
+      line = Enum.map(line, &String.to_integer/1)
+      sum + f.(line)
     end)
   end
 end
