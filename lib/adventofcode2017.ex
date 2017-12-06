1df3303971a60521d7a1c5bb6fc2b526c2eae376
hurley <sean.hurley6@gmail.com>
2017-12-06 16:06:48 -0700

    Day 4 part 2

--- lib/adventofcode2017.ex
+++ lib/adventofcode2017.ex
@@ -7,6 +7,7 @@ defmodule AdventOfCode2017 do
       "2.2" -> Day2.checksum_division("data/day2.txt")
       "3" -> Day3.shortest_path(265149)
       "4" -> Day4.count_valid_passwords("data/day4.txt")
+      "4.2" -> Day4.count_valid_passwords_part_2("data/day4.txt")
     end
 
     IO.inspect result
--- lib/adventofcode2017/day4.ex
+++ lib/adventofcode2017/day4.ex
@@ -10,4 +10,17 @@ defmodule Day4 do
       length(line) == length(Enum.uniq(line))
     end)
   end
+
+  def count_valid_passwords_part_2(file) do
+    {:ok, contents} = File.read(file)
+
+    String.trim(contents)
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&(String.split(&1, " ")))
+    |> Enum.map(fn(line) -> Enum.map(line, &(Enum.sort(String.graphemes(&1)))) end)
+    |> Enum.count(fn(line) ->
+      length(line) == length(Enum.uniq(line))
+    end)
+  end
 end
