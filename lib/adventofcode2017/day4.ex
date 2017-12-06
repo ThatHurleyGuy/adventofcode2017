e5bdae1e7ada630daf715c894b1481026af6bfde
hurley <sean.hurley6@gmail.com>
2017-12-06 16:09:48 -0700

    Refactor day 4

--- lib/adventofcode2017/day4.ex
+++ lib/adventofcode2017/day4.ex
@@ -1,24 +1,22 @@
 defmodule Day4 do
   def count_valid_passwords(file) do
-    {:ok, contents} = File.read(file)
-
-    String.trim(contents)
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&(String.split(&1, " ")))
-    |> Enum.count(fn(line) ->
-      length(line) == length(Enum.uniq(line))
-    end)
+    count_valid_passwords(file, &(&1))
   end
 
   def count_valid_passwords_part_2(file) do
+    count_valid_passwords(file, fn(line) ->
+      Enum.map(line, &(Enum.sort(String.graphemes(&1))))
+    end)
+  end
+
+  defp count_valid_passwords(file, transform_line) do
     {:ok, contents} = File.read(file)
 
     String.trim(contents)
     |> String.split("\n")
     |> Enum.map(&String.trim/1)
     |> Enum.map(&(String.split(&1, " ")))
-    |> Enum.map(fn(line) -> Enum.map(line, &(Enum.sort(String.graphemes(&1)))) end)
+    |> Enum.map(&(transform_line.(&1)))
     |> Enum.count(fn(line) ->
       length(line) == length(Enum.uniq(line))
     end)
