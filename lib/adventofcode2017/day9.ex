ce258f4a606b598dd2aaba30cd983dd5c6520357
hurley <sean.hurley6@gmail.com>
2017-12-10 18:53:15 -0700

    Day 9 refactor

--- lib/adventofcode2017/day9.ex
+++ lib/adventofcode2017/day9.ex
@@ -1,36 +1,38 @@
 defmodule Day9 do
   def groups(file) do
-		{:ok, contents} = File.read(file)
-		String.trim(contents)
-    |> String.graphemes
+    parse_file(file)
     |> count_groups
   end
 
   def garbage(file) do
-		{:ok, contents} = File.read(file)
-		String.trim(contents)
-    |> String.graphemes
+    parse_file(file)
     |> count_garbage
   end
 
-  def count_groups(rest, contained \\ 0)
-  def count_groups(["{" | rest], contained), do: count_groups(rest, contained + 1)
-  def count_groups(["}", "," | rest], contained), do: contained + count_groups(rest, contained - 1)
-  def count_groups(["}" | rest], contained), do: contained + count_groups(rest, contained-1)
-  def count_groups(["<" | rest], contained), do: remove_garbage(rest, contained)
-  def count_groups(["," | rest], contained), do: count_groups(rest, contained)
-  def count_groups([], _), do: 0
+  defp parse_file(file) do
+		{:ok, contents} = File.read(file)
+		String.trim(contents)
+    |> String.graphemes
+  end
 
-  def remove_garbage([">" | rest], contained),do: count_groups(rest, contained)
-  def remove_garbage(["!", _ | rest], contained), do: remove_garbage(rest, contained)
-  def remove_garbage([_ | rest], contained), do: remove_garbage(rest, contained)
+  defp count_groups(rest, contained \\ 0)
+  defp count_groups(["{" | rest], contained), do: count_groups(rest, contained + 1)
+  defp count_groups(["}", "," | rest], contained), do: contained + count_groups(rest, contained - 1)
+  defp count_groups(["}" | rest], contained), do: contained + count_groups(rest, contained-1)
+  defp count_groups(["<" | rest], contained), do: remove_garbage(rest, contained)
+  defp count_groups(["," | rest], contained), do: count_groups(rest, contained)
+  defp count_groups([], _), do: 0
 
-  def count_garbage(rest, contained \\ 0)
-  def count_garbage(["<" | rest], contained), do: count_inner_garbage(rest, contained)
-  def count_garbage([_ | rest], contained), do: count_garbage(rest, contained)
-  def count_garbage([], contained), do: contained
+  defp remove_garbage([">" | rest], contained),do: count_groups(rest, contained)
+  defp remove_garbage(["!", _ | rest], contained), do: remove_garbage(rest, contained)
+  defp remove_garbage([_ | rest], contained), do: remove_garbage(rest, contained)
 
-  def count_inner_garbage([">" | rest], contained),do: count_garbage(rest, contained)
-  def count_inner_garbage(["!", _ | rest], contained), do: count_inner_garbage(rest, contained)
-  def count_inner_garbage([_ | rest], contained), do: count_inner_garbage(rest, contained + 1)
+  defp count_garbage(rest, contained \\ 0)
+  defp count_garbage(["<" | rest], contained), do: count_inner_garbage(rest, contained)
+  defp count_garbage([_ | rest], contained), do: count_garbage(rest, contained)
+  defp count_garbage([], contained), do: contained
+
+  defp count_inner_garbage([">" | rest], contained),do: count_garbage(rest, contained)
+  defp count_inner_garbage(["!", _ | rest], contained), do: count_inner_garbage(rest, contained)
+  defp count_inner_garbage([_ | rest], contained), do: count_inner_garbage(rest, contained + 1)
 end
