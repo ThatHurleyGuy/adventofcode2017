cd38d06911fac1b0c18b3c888d0ba6e57f55d143
hurley <sean.hurley6@gmail.com>
2018-03-19 21:52:45 -0600

    Day 3 part 2

--- lib/adventofcode2017/day3.ex
+++ lib/adventofcode2017/day3.ex
@@ -1,16 +1,33 @@
 defmodule Day3 do
   def shortest_path(index) do
-    {:ok, {x, y}} =
+    {:ok, {{x, y}, map}} =
       0
       |> Stream.iterate(&(&1 + 1))
       |> Stream.flat_map(&build_level/1)
-      |> Stream.scan({0,0}, fn({step_x, step_y}, {current_x, current_y}) ->
+      |> Stream.scan({{0,0}, %{}}, fn({step_x, step_y}, {{current_x, current_y}, map}) ->
         next = {step_x + current_x, step_y + current_y}
-        next
+        next_value = neighbors(next)
+        |> Enum.map(fn({x,y}) -> map[{x,y}] || 0 end)
+        |> Enum.sum
+
+        next_value = if next_value == 0 do
+          IO.inspect 1
+        else
+          next_value
+        end
+        map = Map.put(map, next, next_value)
+        {next, map}
       end)
       |> Enum.fetch(index-1)
 
-    abs(x) + abs(y)
+    part2 = Enum.map(map, fn({_, value}) ->
+      value
+    end)
+    |> Enum.sort
+    |> IO.inspect
+    |> Enum.find(0, fn(value) -> value > index end)
+
+    {abs(x) + abs(y), part2}
   end
 
   def build_level(0), do: [{0,0}]
@@ -22,5 +39,18 @@ defmodule Day3 do
       Enum.map(1..size, fn _ -> {-1, 0} end) ++
       Enum.map(1..size, fn _ -> {0, -1} end) ++
       Enum.map(1..size, fn _ -> {1, 0} end)
+  end
+
+  defp neighbors({x, y}) do
+    [
+      {x+1, y},
+      {x+1, y+1},
+      {x+1, y-1},
+      {x-1, y},
+      {x-1, y-1},
+      {x-1, y+1},
+      {x, y+1},
+      {x, y-1},
+    ]
   end
 end
